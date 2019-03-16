3e92a99b2f34d52192ca7e4e6d248cb22bb27ffe
softartisan <slcaniob@gmail.com>
2019-03-16 17:50:46 -0300

    first commit

---
+++ todo-app.js
@@ -0,0 +1,80 @@
+const todos = 
+[
+    {
+        text: 'Estudiar ingles',
+        completed: true
+    },
+    {
+        text: 'Estudiar Javascript',
+        completed: true
+    },
+    {
+        text: 'Estudiar Matematicas',
+        completed: false
+    },
+    {
+        text: 'Limpiar el perro',
+        completed: false
+    }
+];
+
+const insertTodos = (todos) => {
+    const ul = document.createElement('ul');
+    todos.forEach((todo) => {
+        const li = document.createElement('li');
+        const pText = document.createElement('p');
+        const pCompleted = document.createElement('p');
+
+        pText.textContent = todo.text;
+        pCompleted.textContent = todo.completed ? 'Completada: Sí' : 'Completada: No';
+
+        const classForCompleted = todo.completed ? 'completed' : 'notCompleted';
+
+        pCompleted.classList.add(classForCompleted);
+        pText.classList.add('text');
+
+        li.appendChild(pText);
+        li.appendChild(pCompleted);
+        ul.appendChild(li);
+        document.querySelector('#divTodo').appendChild(ul);
+    });
+}
+
+const removeTodos = () =>{
+    const lis = document.querySelectorAll('#divTodo li');
+    lis.forEach((li)=>{
+        li.remove();
+    });
+}
+
+const addTodo = (text,completed) =>{
+    const todo = {text,completed};
+    todos.push(todo);
+    removeTodos();
+    insertTodos(todos);
+}
+
+const renderFilteredTodos = (todos,filters) => {
+    const filteredTodos = todos.filter((todo)=>{
+        const textToFilter = todo.text.toLowerCase();
+        return textToFilter.includes(filters.toLowerCase());
+    });
+    removeTodos();
+    insertTodos(filteredTodos);
+}
+
+
+insertTodos(todos);
+
+document.querySelector('#divSearch').addEventListener('input',(e)=>{
+    renderFilteredTodos(todos,e.target.value);
+});
+
+document.querySelector('#buttonAdd').addEventListener('click',() => {
+    const inputAdd = document.querySelector('#inputAdd');
+    addTodo(inputAdd.value,true);
+});
+
+
+
+
